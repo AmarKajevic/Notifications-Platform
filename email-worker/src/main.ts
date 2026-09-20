c80b0c27b6ce2d65e2bfe2c59b918c9e3a0af60d
Amar <kajevicamar997@gmail.com>
2026-09-20 20:45:31 +0200

    connection email-worker to kafka

--- email-worker/src/main.ts
+++ email-worker/src/main.ts
@@ -1,21 +1,43 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import 'dotenv/config';
 
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app/app.module';
 
+function getKafkaBrokers(): string[] {
+  const brokers = process.env.KAFKA_BROKERS;
+
+  if (!brokers) {
+    throw new Error('KAFKA_BROKERS is not defined');
+  }
+
+  return brokers.split(',');
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'email-worker',
+          brokers: getKafkaBrokers(),
+        },
+
+        consumer: {
+          groupId: 'email-worker-group',
+        },
+
+        subscribe: {
+          fromBeginning: true,
+        },
+      },
+    },
   );
+
+  await app.listen();
 }
 
 bootstrap();
